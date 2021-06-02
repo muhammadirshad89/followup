5550fcb0c93705cb3214279aca929e756e90090f
muhammad irshad <syyed.asshi89@gmail.com>
2021-06-02 14:34:18 +0500

    first modified

---
+++ config.php
@@ -0,0 +1,29 @@
+<?php
+// +------------------------------------------------------------------------+
+// | @author Deen Doughouz (DoughouzForest)
+// | @author_url 1: http://www.quickdate.com
+// | @author_url 2: http://codecanyon.net/user/doughouzforest
+// | @author_email: wowondersocial@gmail.com
+// +------------------------------------------------------------------------+
+// | Quickdate - The Ultimate PHP Dating Platform
+// | Copyright (c) 2018 Quickdate. All rights reserved.
+// +------------------------------------------------------------------------+
+// MySQL Hostname
+$servername = "localhost";
+// MySQL Database User
+$dbusername = "root";
+// MySQL Database Password
+$dbpassword = "123";
+// MySQL Database Name
+$dbname = "quickdate";
+
+// Site URL
+$site_url = "http://localhost/quickdate"; // e.g (http://example.com)
+
+$app = "QuickDating";
+$endpoint_v = "v1";
+$serverkey = "00c678c44b31b495aa1dcd9893433e4f";
+
+// Purchase code
+$purchase_code = "121232323"; // Your purchase code, don't give it to anyone.
+?>
